986f16b7f6ad8ebe9f354362cf8259eb769c789d
Andrey Yamanov <tenphi@gmail.com>
2015-07-05 19:17:39 +0300

    remove unused vars

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var nodemon = require('nodemon');
 var path = require('path');
 
 var isProduction = process.env.NODE_ENV === 'production';
-var host = process.env.APP_HOST || 'localhost';
-var port = isProduction ? 8080 : 3000;
 
 // We only want to run the workflow when not in production
 if (!isProduction) {
